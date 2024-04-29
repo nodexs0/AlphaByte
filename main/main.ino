dd3a988ee9fa3a0d119486ff089d4b2afbb37bdc
nodexs0 <nodexs@outlook.com>
2024-04-29 12:58:36 -0600

    Encender flash del esp32

--- main/main.ino
+++ main/main.ino
@@ -1,21 +1,34 @@
 #include "WifiCam.hpp"
 #include <WiFi.h>
 #include <WebServer.h>
+#include <ArduinoJson.h>
 
 static const char* WIFI_SSID = "**";
 static const char* WIFI_PASS = "**";
+
+static const IPAddress STATIC_IP(192, 168, 137, 72);
+static const IPAddress GATEWAY(192, 168, 1, 1);
+static const IPAddress SUBNET(255, 255, 255, 0);
+
+int pin = 4;
 
 esp32cam::Resolution initialResolution;
 
 WebServer server(80);
 
 void setup() {
+  
+  pinMode(pin, OUTPUT);
 
   Serial.begin(115200);
   delay(2000);
 
   WiFi.persistent(false);
+
+   // Configura la dirección IP estática
   WiFi.mode(WIFI_STA);
+  WiFi.config(STATIC_IP, GATEWAY, SUBNET);
+
   WiFi.begin(WIFI_SSID, WIFI_PASS);
 
   if (WiFi.waitForConnectResult() != WL_CONNECTED) {
@@ -48,24 +61,45 @@ void setup() {
   Serial.println("Servidor iniciado");
   Serial.print("http://");
   Serial.println(WiFi.localIP());
-
   addRequestHandlers();
   recibeInstrucciones();
   server.begin();
 }
 
-void
-loop()
-{
+void loop() {
   server.handleClient();
 }
-
 
 void recibeInstrucciones() {
   server.on("/instrucciones", HTTP_POST, []() {
     String json = server.arg("plain");
     Serial.print("Instrucciones recibidas: ");
     Serial.println(json);
+
+    // Crear un objeto DynamicJsonDocument para almacenar el JSON recibido
+    DynamicJsonDocument doc(1024); // Tamaño máximo del JSON recibido
+
+    // Deserializar el JSON
+    DeserializationError error = deserializeJson(doc, json);
+
+    // Verificar si hubo un error en la deserialización
+    if (error) {
+      Serial.print("Error al analizar JSON: ");
+      Serial.println(error.c_str());
+      server.send(400, "text/plain", "Error al analizar JSON");
+      return;
+    }
+
+    if (doc.containsKey("Luz")) {
+      String comando = doc["Luz"].as<String>();
+      if (comando == "encender") {
+        digitalWrite(pin, HIGH);
+      } else if (comando == "apagar") {
+        digitalWrite(pin, LOW);
+      }
+    } else {
+      Serial.println("Comando no reconocido");
+    }
     
     server.send(200, "text/plain", "Instrucciones recibidas correctamente");
   });
--- src/instrucciones.py
+++ src/instrucciones.py
@@ -1,24 +1,34 @@
 import requests
 
 # Dirección IP de tu ESP32
-ESP32_IP = ''  # Reemplaza con la IP de tu ESP32
+ESP32_IP = ''
 
 # URL a la que enviar la solicitud
 url = f'http://{ESP32_IP}/instrucciones'
 
-# Instrucciones a enviar
-instrucciones = {
-    'comando': 'rotar_servo',
-    'parametros': {
-        'angulo': 90
+def enviar_instrucciones(instrucciones):
+    # Enviar solicitud POST con las instrucciones
+    response = requests.post(url, json=instrucciones)
+
+    # Verificar si la solicitud fue exitosa
+    if response.status_code == 200:
+        print("Instrucciones enviadas correctamente")
+    else:
+        print("Error al enviar instrucciones:", response.status_code)
+
+def crear_instrucciones(luz):
+    instrucciones = {
+        'Luz': luz,
     }
-}
+    return instrucciones
 
-# Enviar solicitud POST con las instrucciones
-response = requests.post(url, json=instrucciones)
-
-# Verificar si la solicitud fue exitosa
-if response.status_code == 200:
-    print("Instrucciones enviadas correctamente")
-else:
-    print("Error al enviar instrucciones:", response.status_code)
+while True:
+    entrada = int(input(""))
+    if entrada == 1:
+        instrucciones = crear_instrucciones("encender")
+        enviar_instrucciones(instrucciones)
+    elif entrada == 2:
+        instrucciones = crear_instrucciones("apagar")
+        enviar_instrucciones(instrucciones)
+    else:
+        break
